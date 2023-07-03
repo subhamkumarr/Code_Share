b26a1e351d6adb2fd2f47b21624c11beb2246b70
subhamkumarr <kumarsubham75861@gmail.com>
2023-07-04 02:20:54 +0530

    added code area to write code + left sidebar

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import Client from "../components/Client";
+import Editor from "../components/Editor";
 
 const EditorPage = () => {
-  return (
-    <div>EditorPage</div>
-  )
-}
+  const [clients, setClients] = useState([
+    { socketId: 1, username: "Subham K" },
+    { socketId: 2, username: "Sanket S" },
+    { socketId: 3, username: "Striver" },
+  ]);
 
-export default EditorPage
+  return (
+    <div className="mainWrap">
+      <div className="aside">
+        <div className="asideInner">
+          <div className="logo">
+            <img className="logoImage" src="/logo.png" alt="logo" />
+          </div>
+          <h3>Connected</h3>
+          <div className="clientsList">
+            {clients.map((client) => (
+              <Client 
+              key={client.socketId} 
+              username={client.username} 
+              />
+            ))}
+          </div>
+        </div>
+        <button className="btn copyBtn"> Copy ROOM ID</button>
+        <button className="btn leaveBtn"> Leave</button>
+      </div>
+      <div className="editorWrap">
+        <Editor />
+      </div>
+    </div>
+  );
+};
+
+export default EditorPage;
