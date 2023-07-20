7166e58623f27819695271188bd9e5ba63296253
subhamkumarr <kumarsubham75861@gmail.com>
2023-07-21 01:52:15 +0530

    deployment setup

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-// const path = require('path');
+const path = require('path');
 const { Server } = require('socket.io');
 const ACTIONS = require('./src/Actions');
 
 const server = http.createServer(app);
 const io = new Server(server);
 
-// app.use(express.static('build'));
-// app.use((req, res, next) => {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+ app.use(express.static('build'));
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
  const userSocketMap = {};
 function getAllConnectedClients(roomId) {
