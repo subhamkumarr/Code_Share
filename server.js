6e22404ca6e65d3a64ab3f8e82bd4f0e2d2be37d
subhamkumarr <kumarsubham75861@gmail.com>
2023-07-27 11:42:49 +0530

    Update server.js file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const ACTIONS = require('./src/Actions');
 const server = http.createServer(app);
 const io = new Server(server);
 
- app.use(express.static('build'));
+ app.use(express.static(__dirname + '/build/'));
 app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
