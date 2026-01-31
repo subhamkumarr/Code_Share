b2a0e358828187e7226c26fdb1bdff74006d6df2
subhamkumarr <kumarsubham75861@gmail.com>
2026-01-31 11:37:21 +0530

    Debug: Add visual probe (red box) and white background to whiteboard

--- src/components/Board.js
+++ src/components/Board.js
@@ -119,7 +119,19 @@ const Board = ({ socketRef, roomId }) => {
     }, [editor, socket, roomId]);
 
     return (
-        <div style={{ position: 'absolute', inset: 0, width: '100%', height: '100%' }}>
+        <div style={{ position: 'absolute', inset: 0, width: '100%', height: '100%', backgroundColor: 'white' }}>
+            {/* Visual Probe to check if container is rendering and has size */}
+            <div style={{
+                position: 'absolute',
+                top: 10,
+                left: 10,
+                width: 50,
+                height: 50,
+                backgroundColor: 'red',
+                zIndex: 9999,
+                pointerEvents: 'none'
+            }} />
+
             <ErrorBoundary>
                 <Tldraw
                     onMount={handleMount}
